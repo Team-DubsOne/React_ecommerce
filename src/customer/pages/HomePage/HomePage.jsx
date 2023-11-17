0f1548a9f817a17c99dae3d102a7db6e4f23b93d
LOSTCOLE <colebarber977@gmail.com>
2023-11-17 15:43:35 -0600

    new

---
+++ src/customer/pages/HomePage/HomePage.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import MainCrosel from '../../componets/HomeCarosel/MainCrosel'
+
+
+const  HomePage = () => {
+  return (
+    <div>
+      <MainCrosel/>
+      <div>
+        other section
+      </div>
+    </div>
+  )
+}
+
+export default HomePage
