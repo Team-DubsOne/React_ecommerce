3e5c770a55dc81be4df385d39817ae6fafc4336a
LOSTCOLE <colebarber977@gmail.com>
2023-11-17 15:21:27 -0600

    React ecommcere tailwind,

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Navigation from './customer/componets/Navigation/Navigation';
+
+function App() {
+  return (
+    <div className="App">
+      <Navigation/>
+    </div>
+  );
+}
+
+export default App;
